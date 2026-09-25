501dc11a264b849f51d4df140995d4b768a6a816
Fidel Montemira Orozco <zS22013691@estudiantes.uv.mx>
2026-09-25 12:02:04 -0600

    feat: validación de prerrequisitos, bloqueo de modales inválidos y actualización visual de inscripciones

---
+++ Models/AlumnoAvance.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace PlaneaUV_Economia.Models
+{
+    public class AlumnoAvance
+    {
+        public string Matricula { get; set; }
+        public string Nombre { get; set; }
+
+        public List<MateriaCursada> Materias { get; set; } = new List<MateriaCursada>();
+    }
+
+    public class MateriaCursada
+    {
+        public string Nombre { get; set; }
+        public int Creditos { get; set; }
+        public string TipoInscripcion { get; set; } 
+        public string UltimoExamen { get; set; }  
+        public string Estado { get; set; }          
+    }
+}
